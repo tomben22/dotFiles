1ea78008490d4f4e0178228564d07c3c319d573d
Tom Bendig <dev@tomben.de>
2016-03-19 14:18:50 +0100

    minor changes

--- custom_install.sh
+++ custom_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@" #check for sudo
+
+
 apt-get update # get the latest package list
 
 #install brew
@@ -11,5 +14,13 @@ brew install node --with-full-icu
 # install ionic & cordova
 npm install -g cordova ionic
 
+#install cocoapods
+sudo gem cleanup
+sudo gem install cocoapods
+
+#install fastlane stack
+xcode-select --install
+sudo gem install fastlane --verbose
+sudo gem install deliver snapshot frameit sigh pem cert scan gym match pilot
 
 #system install script
