47d1e4ef45829b4cf779f00b6c4d9f096d0f6123
Thomas Bendig <tbendig@csc.com>
2016-03-19 23:40:12 +0100

    minor changes

--- custom_install.sh
+++ custom_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@" #check for sudo
+#[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@" #check for sudo
 
 # install Xcode dev tools
 open -a safari https://developer.apple.com/xcode/download/
@@ -8,7 +8,7 @@ xcode-select --install
 while true; do
 	read -p "Do you installed Xcode & Xcode tools completely ?" yn
 	case $yn in
-		[Yy]* ) exit;;
+		[Yy]* ) break;;
 		[Nn]* ) continue;;
 	* ) echo "Please answer yes or no.";;
 esac
@@ -28,18 +28,22 @@ brew install node --with-full-icu
 # install ionic & cordova
 npm install -g cordova ionic
 
-#install cocoapods
-sudo gem cleanup
-sudo gem install cocoapods
-
-#install fastlane stack
-xcode-select --install
-sudo gem install fastlane --verbose
-sudo gem install deliver snapshot frameit sigh pem cert scan gym match pilot
-
-
 # install wget
 brew install wget
+
+
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@" #check for sudo
+
+#install cocoapods
+#sudo gem cleanup
+gem install cocoapods
+
+#install fastlane stack
+#xcode-select --install
+gem install fastlane
+gem install deliver snapshot frameit sigh pem cert scan gym match pilot
+
+
 
 #system install script
 export TMPDIR=~/tmp
@@ -48,16 +52,16 @@ cd ~/.tmpinstall
 
 # install xca
 curl -L http://downloads.sourceforge.net/project/xca/xca/1.3.2/xca-1.3.2.dmg > xca.dmg
-sudo hdiutil attach xca.dmg
+hdiutil attach xca.dmg
 cp -r /Volumes/`ls /Volumes/ | grep xca`/xca.app /Applications
-sudo hdiutil detach /Volumes/`ls /Volumes/ | grep xca`
+hdiutil detach /Volumes/`ls /Volumes/ | grep xca`
 
 
 # install sublimeText
 curl -L https://download.sublimetext.com/Sublime%20Text%20Build%203103.dmg > Sublime.dmg
-sudo hdiutil attach Sublime.dmg
+hdiutil attach Sublime.dmg
 cp -r /Volumes/Sublime\ Text/Sublime\ Text.app/ /Applications/
-sudo hdiutil detach Sublime.dmg
+hdiutil detach Sublime.dmg
 
 
 
@@ -73,6 +77,7 @@ while true; do
 	* ) echo "Please answer yes or no.";;
 esac
 done
+
 #install sourcetree
 curl -L -# https://downloads.atlassian.com/software/sourcetree/SourceTree_2.2.3.dmg > sourcetree.dmg
 hdiutil attach sourcetree.dmg 
